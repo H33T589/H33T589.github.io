466868fccc671992ee01028b370e09b437f5b162
H33T589 <133909908+H33T589@users.noreply.github.com>
2025-10-27 13:00:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-document.addEventListener("scroll", () => {
-  document.querySelectorAll(".fade-in").forEach((el) => {
+const fadeElems = document.querySelectorAll(".fade-in");
+
+const handleScroll = () => {
+  fadeElems.forEach(el => {
     const rect = el.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
+    if (rect.top < window.innerHeight - 80) {
       el.style.animationPlayState = "running";
     }
   });
-});
+};
+
+window.addEventListener("scroll", handleScroll);
+window.addEventListener("load", handleScroll);
